ccaa26cd69800cebff3a8d5cb0c44bea7d8f1b9b
abruzzim <abruzzim@framework-solutions.com>
2015-04-15 14:05:42 -0400

    Add Main Tab Bar Controller.

--- ACDSprototype3/AppDelegate.m
+++ ACDSprototype3/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"%%AppDelegate-I-TRACE, -application:didFinishLaunchingWithOptions: called.");
     // Override point for customization after application launch.
     return YES;
 }
--- ACDSprototype3/MainTBC.m
+++ ACDSprototype3/MainTBC.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  MainTBC.m
 //  ACDSprototype3
 //
 //  Created by dev1 on 4/15/15.
 //  Copyright (c) 2015 dev1. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "MainTBC.h"
 
-@interface ViewController : UIViewController
-
+@implementation MainTBC
 
 @end
-
