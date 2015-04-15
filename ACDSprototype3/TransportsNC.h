04f916147075092a63e3dc91a3af87e6eb858dff
abruzzim <abruzzim@framework-solutions.com>
2015-04-15 14:23:30 -0400

    Add Transports Navigation Controller Subclass.

---
+++ ACDSprototype3/TransportsNC.h
@@ -0,0 +1,13 @@
+//
+//  TransportsNC.h
+//  ACDSprototype3
+//
+//  Created by dev1 on 4/15/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransportsNC : UINavigationController
+
+@end
---
+++ ACDSprototype3/TransportsNC.m
@@ -0,0 +1,32 @@
+//
+//  TransportsNC.m
+//  ACDSprototype3
+//
+//  Created by dev1 on 4/15/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import "TransportsNC.h"
+
+@implementation TransportsNC
+
+- (void)viewDidLoad {
+    NSLog(@"%%TransportsNC-I-TRACE, -viewDidLoad called.");
+    
+    [super viewDidLoad];
+    self.title = @"Transports";
+    self.tabBarItem.image = [UIImage imageNamed:@"medical52"];
+    self.toolbarHidden = NO;
+    self.toolbar.translucent = YES;
+    self.toolbar.barStyle = UIBarStyleDefault;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    NSLog(@"%%TransportsNC-I-TRACE, -didReceiveMemoryWarning called.");
+    
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
