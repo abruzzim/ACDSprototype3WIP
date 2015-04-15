c54aac07ee608bb3cc5810b1ddf1a1f593f1e2a9
abruzzim <abruzzim@framework-solutions.com>
2015-04-15 14:36:19 -0400

    Add Transports Parent View Controller Subclass.

---
+++ ACDSprototype3/TransportsParentVC.h
@@ -0,0 +1,13 @@
+//
+//  TransportsParentVC.h
+//  ACDSprototype3
+//
+//  Created by dev1 on 4/15/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransportsParentVC : UIViewController
+
+@end
---
+++ ACDSprototype3/TransportsParentVC.m
@@ -0,0 +1,37 @@
+//
+//  TransportsParentVC.m
+//  ACDSprototype3
+//
+//  Created by dev1 on 4/15/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import "TransportsParentVC.h"
+
+@interface TransportsParentVC ()
+
+@end
+
+@implementation TransportsParentVC
+
+- (void)viewDidLoad {
+    NSLog(@"%%TransportsParentVC-I-TRACE, -viewDidLoad called.");
+    
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    // Child 1 VC. -------------------------------------------------------------------------------------|
+    //
+    
+    // Child 2 VC. -------------------------------------------------------------------------------------|
+    //
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
